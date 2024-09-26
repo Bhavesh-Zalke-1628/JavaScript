af61d1161ecf098d3ce3431aa4f682e08f5b5050
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-09-26 13:45:23 +0530

    Data type

---
+++ 01variable.js
@@ -0,0 +1,14 @@
+const accountId = 14145
+let accountEmail = "bhavesh@gmail.com"
+var acccountPassword = "1234"
+accountCity = "katri"
+
+accountEmail = "bz@gmail.com"
+acccountPassword = "14785"
+accountCity = "yavatmal"
+
+/*
+prefer not to used the  var, because of issue of the block scope. 
+*/
+
+console.table([accountId, accountEmail, accountCity, acccountPassword])
---
+++ 02DataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treate all as js code as never version
+
+// alert("hello")
+
+let name = "bhavesh"
+let age = 21
+let isLoggedIn = false
+
+// number
+// string
+// Boolean
+// null  //this a standalone value
+// undefined
+// Symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null)
